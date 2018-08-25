afef2b97f56ad723e8cb05016b59b2cdf436a898
edden <edden87@gmail.com>
2018-08-25 13:27:02 +0800

    Added buy order and buy order add.

---
+++ www/wp-content/themes/uhack/buyer-order-add.php
@@ -0,0 +1,163 @@
+<?php
+/*
+ * Template Name: Buyer's Order Add
+ */
+
+get_header();
+?>
+    <!-- Page-Title -->
+    <div class="row">
+        <div class="col-sm-8 center-page">
+            <div class="page-title-box">
+                <h4 class="page-title">New Order</h4>
+            </div>
+        </div>
+    </div>
+    <!-- end page title end breadcrumb -->
+
+    <div class="row">
+        <div class="col-sm-8 center-page">
+            <form role="form">
+                <div class="form-group">
+                    <label for="exampleInputTitle">Name of your Job Order</label>
+                    <input type="text" class="form-control" id="exampleInputTitle" aria-describedby="titleHelp" placeholder="Order Title">
+                </div>
+                <div class="form-group">
+                    <label for="exampleDemand2">Select Agriculture</label>
+                    <select id="exampleDemand2" class="form-control select2">
+                        <option>Select</option>
+                        <optgroup label="Rice">
+                            <option value="7-tonner">7 Tonner</option>
+                            <option value="jasmin">Jasmin</option>
+                            <option value="brown">Brown</option>
+                            <option value="nfa">NFA</option>
+                            <option value="sinandomeng">Sinandomeng</option>
+                        </optgroup>
+                    </select>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label class="control-label">Enter how much do you need (in KG):</label>
+                            <input id="demo3" type="text" value="20" name="demo3">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="datepicker-autoclose">Estimated Deadline</small></label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="mm/dd/yyyy" id="datepicker-autoclose">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="ion-calendar"></i></span>
+                                </div>
+                            </div><!-- input-group -->
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h5>Destination</h5>
+                        <div class="form-group">
+                            <label for="inputAddress" class="col-form-label">Address</label>
+                            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputAddress2" class="col-form-label">Address 2</label>
+                            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputCity" class="col-form-label">City</label>
+                                <input type="text" class="form-control" id="inputCity" value="Davao City">
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="inputState" class="col-form-label">State</label>
+                                <select id="inputState" class="form-control">
+                                    <option value="davao-del-sur">Davao del Sur</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label for="inputZip" class="col-form-label">Zip</label>
+                                <input type="text" class="form-control" id="inputZip" value="8000">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <p>Delivery Preference:</p>
+                            <div class="checkbox checkbox-circle">
+                                <input id="checkbox7" type="checkbox" checked>
+                                <label for="checkbox7">
+                                    Self-help Delivery
+                                </label>
+                            </div>
+                            <div class="checkbox checkbox-circle">
+                                <input id="checkbox8" type="checkbox" checked>
+                                <label for="checkbox8">
+                                    Trusted Delivery Partner <strong class="ti-shield"></strong>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <p>Farmer Preference:</p>
+                        <div class="radio">
+                            <input type="radio" name="radio" id="radio2" value="multiple" checked>
+                            <label for="radio2">
+                                Allow multiple farmers to commit to an order
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <input type="radio" name="radio" id="radio1" value="bulk">
+                            <label for="radio1">
+                                One farmer will commit to the whole order.
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>Publish Type</p>
+                        <div class="radio">
+                            <input type="radio" name="publish_type" id="publish_type1" value="multiple"
+                                   checked>
+                            <label for="publish_type1">Anyone can bid
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <input type="radio" name="publish_type" id="publish_type2" value="bulk">
+                            <label for="publish_type2">
+                                Invite only
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group m-b-1">
+                            <label class="control-label">Estimated price per KG ratio. <br>
+                                <small><strong>Farmers will still decide on the price.</strong></small>
+                            </label>
+                            <input id="demo2" type="text" value="1" name="demo2" class=" form-control">
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>Order Description (Optional):</p>
+                        <textarea class="form-control" rows="5"></textarea>
+                        <br>
+                        <button type="button" class="btn btn-success waves-effect waves-light">Post Order</button>
+                        <button type="button" class="btn btn-default waves-effect waves-light">Cancel</button>
+                        <br>
+                        <br>
+                    </div>
+                </div>
+            </form>
+        </div><!-- end col-->
+    </div>
+<?php get_footer();
---
+++ www/wp-content/themes/uhack/buyer-order.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Template Name: Buyer's Order
+ */
+get_header();
+?>
+    <!-- Page-Title -->
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title">Orders</h4>
+            </div>
+        </div>
+    </div>z
+    <!-- end page title end breadcrumb -->
+
+    <div class="row">
+        <div class="col-sm-4">
+            <a href="order-add.html" class="btn btn-sm btn-primary waves-effect waves-light mb-4">
+                <i class="mdi mdi-plus-circle"></i> Add New
+            </a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card-box">
+                <div class="text-center">
+                    <h2>#5679</h2>
+                </div>
+
+                <p class="font-14 text-center text-muted">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, eaque assumenda doloribus aut mollitia perferendis in perspiciatis fugit explicabo labore!
+                </p>
+
+                <div class="text-center">
+                    <a href="javascript:void(0);" class="btn btn-sm btn-light">View more info</a>
+                </div>
+
+                <div class="row mt-5 text-center">
+                    <div class="col-6">
+                        <h5 class="font-normal text-muted">Responses</h5>
+                        <h3 class="m-b-30">0</h3>
+                    </div>
+                    <div class="col-6">
+                        <h5 class="font-normal text-muted">Budget</h5>
+                        <h3 class="m-b-30">$1540</h3>
+                    </div>
+                </div>
+            </div>
+        </div><!-- end col -->
+    </div>
+
+<?php get_footer();
