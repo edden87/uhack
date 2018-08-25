bcc4d1ee061c6eb7b8915655923e77966628f0d4
edden <edden87@gmail.com>
2018-08-25 13:28:17 +0800

    Added farmer suggestion

---
+++ www/wp-content/themes/uhack/farmer-suggestion.php
@@ -0,0 +1,120 @@
+<?php
+/*
+ * Template Name: Farmer Suggestion
+ */
+get_header();
+?>
+    <div class="row">
+        <div class="col-md-10 center-page m-t-40 center-page">
+            <h3>We found farmers base from your Job Order! <button type="button" class="btn btn-success waves-effect waves-light">Send them an invite</button> <button type="button" class="btn btn-default waves-effect waves-light">No Thanks!</button></h3>
+            <hr>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card-box">
+                        <div class="switchery-demo">
+                            <input type="checkbox" checked data-plugin="switchery" data-color="#00b19d"/>
+                        </div>
+                        <div class="text-center">
+                            <img src="<?= get_stylesheet_directory_uri(); ?>/assets/images/avatar-1.jpg" alt="logo" class="thumb-lg rounded-circle mb-3">
+                            <h4 class="mb-1">Farmer 1</h4>
+                            <p class="text-muted  font-14">San Francisco, California</p>
+                        </div>
+
+                        <p class="font-14 text-center text-muted">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, iure dignissimos fugiat quae quis dicta placeat libero eaque accusamus laborum.
+                        </p>
+
+                        <div class="text-center">
+                            <a href="javascript:void(0);" class="btn btn-sm btn-light">View more info</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card-box">
+                        <div class="switchery-demo">
+                            <input type="checkbox" checked data-plugin="switchery" data-color="#00b19d"/>
+                        </div>
+                        <div class="text-center">
+                            <img src="<?= get_stylesheet_directory_uri(); ?>/assets/images/users/avatar-2.jpg" alt="logo" class="thumb-lg rounded-circle mb-3">
+                            <h4 class="mb-1">Farmer 2</h4>
+                            <p class="text-muted  font-14">San Francisco, California</p>
+                        </div>
+
+                        <p class="font-14 text-center text-muted">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, ad, rerum excepturi quam labore in rem aliquid. Deleniti, non, praesentium!
+                        </p>
+
+                        <div class="text-center">
+                            <a href="javascript:void(0);" class="btn btn-sm btn-light">View more info</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-md-4">
+                    <div class="card-box">
+                        <div class="switchery-demo">
+                            <input type="checkbox" checked data-plugin="switchery" data-color="#00b19d"/>
+                        </div>
+                        <div class="text-center">
+                            <img src="<?= get_stylesheet_directory_uri(); ?>/assets/images/users/avatar-3.jpg" alt="logo" class="thumb-lg rounded-circle mb-3">
+                            <h4 class="mb-1">Farmer 3</h4>
+                            <p class="text-muted  font-14">San Francisco, California</p>
+                        </div>
+
+                        <p class="font-14 text-center text-muted">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem aut quae error quisquam aliquam eius rerum deserunt quaerat expedita assumenda.
+                        </p>
+
+                        <div class="text-center">
+                            <a href="javascript:void(0);" class="btn btn-sm btn-light">View more info</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div><!-- end container -->
+</div><!-- end wrapper -->
+<!-- Footer -->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                2016 - 2018 © Minton - Coderthemes.com
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- End Footer -->
+
+
+<!-- jQuery  -->
+<script src="<?= get_stylesheet_directory_uri(); ?>/assets/js/popper.min.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/assets/js/bootstrap.min.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/assets/js/waves.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/assets/js/jquery.slimscroll.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/assets/js/jquery.scrollTo.min.js"></script>
+
+<script src="<?= get_stylesheet_directory_uri(); ?>/plugins/switchery/switchery.min.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js"></script>
+<script type="text/javascript" src="<?= get_stylesheet_directory_uri(); ?>/plugins/multiselect/js/jquery.multi-select.js"></script>
+<script type="text/javascript" src="<?= get_stylesheet_directory_uri(); ?>/plugins/jquery-quicksearch/jquery.quicksearch.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/plugins/select2/js/select2.min.js" type="text/javascript"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/plugins/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js" type="text/javascript"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/plugins/bootstrap-inputmask/bootstrap-inputmask.min.js" type="text/javascript"></script>
+
+<script src="<?= get_stylesheet_directory_uri(); ?>/plugins/moment/moment.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/plugins/mjolnic-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/plugins/custombox/dist/custombox.min.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/assets/pages/jquery.companies.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/assets/pages/jquery.form-advanced.init.js"></script>
+
+<!-- App js -->
+<script src="<?= get_stylesheet_directory_uri(); ?>/assets/js/jquery.core.js"></script>
+<script src="<?= get_stylesheet_directory_uri(); ?>/assets/js/jquery.app.js"></script>
+
+</body>
+</html>
